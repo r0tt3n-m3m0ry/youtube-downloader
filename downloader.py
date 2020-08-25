7eb9f7e273c09391c53a227d6f7d6e32833f647b
Oleg <r0tt3n-m3m0ry@protonmail.com>
2020-08-25 05:03:00 +0300

    Some updates

--- downloader.py
+++ downloader.py
@@ -9,27 +9,38 @@ except:
 
 import os
 
-with open(input('Enter name of file which contain the videos data: ')) as file_with_links:
-    for file_string in file_with_links:
-        if file_string[0] == '\n' or file_string[0] == ' ' or file_string[0] == '#':
-            pass
-        else:
-            link = file_string.split(' <<>> ')[0]
-            author = file_string.split(' <<>> ')[1]
-            content_type = file_string.split(' <<>> ')[-1].strip()
-
-            try:
-                os.mkdir(author)
-            except OSError:
+try:   
+    with open(input('Enter name of file which contain the videos data: ')) as file_with_links:
+        for file_string in file_with_links:
+            if file_string[0:4] != 'http': # if first four symbols in string not 'http', this is not link -> string is incorrect
                 pass
-
-            os.chdir(author)
-
-            if content_type == 'audio':
-                os.system(f'youtube-dl -i --extract-audio --audio-format mp3 {link}')
-            if content_type == 'video':
-                os.system(f'youtube-dl -i {link}')
             else:
-                pass
+                if len(file_string.split('<<>>')) != 4: # if after split() count of elements in string not four, string is incorrect
+                    continue
 
-            os.chdir('..')
+                link = file_string.split('<<>>')[0].strip()
+                content_type = file_string.split('<<>>')[1].strip()
+                video_category = file_string.split('<<>>')[-2].strip()
+                author = file_string.split('<<>>')[-1].strip()
+
+                try:
+                    os.makedirs(f'{video_category}\\{author}') if os.name == 'nt' else os.makedirs(f'{video_category}/{author}')
+                except OSError:
+                    pass
+
+                os.chdir(f'{video_category}\\{author}') if os.name == 'nt' else os.chdir(f'{video_category}/{author}')
+
+                if content_type == 'audio':
+                    os.system(f'youtube-dl -i --extract-audio --embed-thumbnail --audio-format mp3 {link}')
+                elif content_type == 'video':
+                    os.system(f'youtube-dl -i {link}')
+                else:
+                    pass
+
+                os.chdir('..\\..') if os.name == 'nt' else os.chdir('../..')
+
+except FileNotFoundError:
+    print('File with this name not found!'); exit()
+
+except KeyboardInterrupt:
+    print('Goodbye!'); exit()
